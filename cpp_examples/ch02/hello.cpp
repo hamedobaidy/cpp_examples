fabb660ffc70efe1e941272e1481755270c8dd37
hamedobaidy <hamed.obaidy@gmail.com>
2015-07-09 13:05:39 +0430

    Commit project cpp_examples for the first time

---
+++ cpp_examples/ch02/hello.cpp
@@ -0,0 +1,16 @@
+/*
+ * hello.cpp
+ *
+ *  Created on: Jul 8, 2015
+ *      Author: hamed
+ */
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	std::cout << "Hello, World!\n";
+
+	return 0;
+}
+
+
---
+++ cpp_examples/cpp_examples.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cpp_examples.cpp
+// Author      : Hamed Mohammdi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World");
+	return EXIT_SUCCESS;
+}
