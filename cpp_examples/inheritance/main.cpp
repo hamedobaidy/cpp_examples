3bd49b2298e3b945714071f6da3036e299648707
hamedobaidy <hamed.obaidy@gmail.com>
2015-07-19 01:06:40 +0430

    inheritance

---
+++ cpp_examples/inheritance/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jul 19, 2015
+ *      Author: hamed
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char **argv) {
+
+	return EXIT_SUCCESS;
+}
+
