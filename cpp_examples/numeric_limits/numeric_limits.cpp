4e20ee8cc92cff20d2784431d7f73b02e0652b3f
hamedobaidy <hamed.obaidy@gmail.com>
2015-07-23 19:49:51 +0430

    numeric_limits example

--- cpp_examples/numeric_limits/numeric_limits.cpp
+++ cpp_examples/numeric_limits/numeric_limits.cpp
@@ -16,6 +16,17 @@ int main () {
   std::cout << "int is signed: " << std::numeric_limits<int>::is_signed << '\n';
   std::cout << "Non-sign bits in int: " << std::numeric_limits<int>::digits << '\n';
   std::cout << "int has infinity: " << std::numeric_limits<int>::has_infinity << '\n';
+  std::cout << "Maximum value for float: " << std::numeric_limits<float>::max() << '\n';
+  std::cout << "Minimum value for float: " << std::numeric_limits<float>::min() << '\n';
+  std::cout << "Maximum value for double: " << std::numeric_limits<double>::max() << '\n';
+  std::cout << "Maximum vlaue for long long int: " << std::numeric_limits<long long int>::max() << '\n';
+  std::cout << "Double binary digits: " << std::numeric_limits<double>::digits << '\n';
+  std::cout << "Double digits: "<< std::numeric_limits<double>::digits10 << '\n';
+  std::cout << "long double digits: " << std::numeric_limits<long double>::digits10 << '\n';
+  std::cout << "Maximum value for long double: " << std::numeric_limits<long double>::max() << '\n';
+  std::cout << "long double epsilon: " << std::numeric_limits<long double>::epsilon() << '\n';
+  std::cout << "double epsilon: " << std::numeric_limits<double>::epsilon() << '\n';
+
   return 0;
 }
 
